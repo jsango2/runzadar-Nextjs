70b8d873caf9b87b73f3e56977e3617d2fbe6ccb
jsango2 <jure.sango@gmail.com>
2022-01-01 23:50:43 +0100

    first commit

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -6,106 +6,36 @@ import { getAllPostsWithSlug, getPostAndMorePosts } from "../../lib/api";
 import PostTitle from "../../components/post-title";
 import Head from "next/head";
 import Image from "next/image";
-import styled from "styled-components";
-
-// import { HeroWrap } from "./style.js"
-const WrapAll = styled.div`
-  position: relative;
-  width: 100%;
-  height: 625px;
-  padding-top: 50px;
-  z-index: 3;
-  overflow: hidden;
-  background-color: #151520;
-`;
-
-const Naslov = styled.div`
-  position: relative;
-  font-style: italic;
-  width: 700px;
-  font-weight: 800;
-  font-size: 72px;
-  line-height: 120%;
-  margin-top: 0;
-  @media only screen and (max-width: 430px) {
-    font-size: 36px;
-    width: 90%;
-  }
-`;
-
-const WrapNaslovButton = styled.div`
-  position: absolute;
-  width: 868px;
-  height: auto;
-  bottom: 8%;
-  /* left: 16%; */
-  margin: 0 auto;
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-between;
-  @media only screen and (max-width: 430px) {
-    font-size: 36px;
-    width: 90%;
-    margin: 0 auto 0 30px;
-  }
-`;
-const WrapText = styled.div`
-  margin: 20px auto 0 auto;
-  width: 868px;
-  font-size: 21px;
-  @media only screen and (max-width: 430px) {
-    width: 90%;
-  }
-`;
-
-const Overlay = styled.div`
-  position: absolute;
-  z-index: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  background-color: rgb(0 0 0 / 20%);
-`;
-const Text = styled.div`
-  font-size: 21px;
-  line-height: 142.5%;
-  & img {
-    margin: 0 auto;
-    width: 100%;
-    object-fit: cover;
-  }
-  & p {
-    margin: 1.5rem 0;
-  }
-  @media only screen and (max-width: 430px) {
-    font-size: 16px;
-  }
-`;
+import {
+  WrapNaslovButton,
+  Naslov,
+  WrapText,
+  Overlay,
+  Text,
+} from "../../styles/postStyles";
 
 export default function Post({ post, posts, preview }) {
   const router = useRouter();
-  const morePosts = posts?.edges;
-
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
   }
-  // const post = data.wpgraphql.post;
   const [datum, setDatum] = useState("-");
-  useEffect(() => {
-    let datumPosta = post.date;
-    function formatDate(date) {
-      var d = new Date(date),
-        month = "" + (d.getMonth() + 1),
-        day = "" + d.getDate(),
-        year = d.getFullYear();
+  // useEffect(() => {
+  //   let datumPosta = post.date;
+  //   function formatDate(date) {
+  //     var d = new Date(date),
+  //       month = "" + (d.getMonth() + 1),
+  //       day = "" + d.getDate(),
+  //       year = d.getFullYear();
 
-      if (month.length < 2) month = "0" + month;
-      if (day.length < 2) day = "0" + day;
+  //     if (month.length < 2) month = "0" + month;
+  //     if (day.length < 2) day = "0" + day;
 
-      return [day, month, year].join(".");
-    }
-    setDatum(formatDate(datumPosta));
-  }, [post.date]);
+  //     return [day, month, year].join(".");
+  //   }
+  //   setDatum(formatDate(datumPosta));
+  // }, [post.date]);
+
   return (
     <Layout>
       {router.isFallback ? (
@@ -161,9 +91,9 @@ export default function Post({ post, posts, preview }) {
               }}
             >
               <div style={{ fontSize: "0.8rem" }}>Autor: RunZadar.com</div>
-              <div style={{ fontSize: "0.8rem", marginRight: "1rem" }}>
+              {/* <div style={{ fontSize: "0.8rem", marginRight: "1rem" }}>
                 {datum}
-              </div>
+              </div> */}
             </div>
             <div />
             <div
---
+++ styles/postStyles.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+// import { HeroWrap } from "./style.js"
+export const WrapAll = styled.div`
+  position: relative;
+  width: 100%;
+  height: 625px;
+  padding-top: 50px;
+  z-index: 3;
+  overflow: hidden;
+  background-color: #151520;
+`;
+
+export const Naslov = styled.div`
+  position: relative;
+  font-style: italic;
+  width: 700px;
+  font-weight: 800;
+  font-size: 72px;
+  line-height: 120%;
+  margin-top: 0;
+  @media only screen and (max-width: 430px) {
+    font-size: 36px;
+    width: 90%;
+  }
+`;
+
+export const WrapNaslovButton = styled.div`
+  position: absolute;
+  width: 868px;
+  height: auto;
+  bottom: 8%;
+  /* left: 16%; */
+  margin: 0 auto;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  @media only screen and (max-width: 430px) {
+    font-size: 36px;
+    width: 90%;
+    margin: 0 auto 0 30px;
+  }
+`;
+export const WrapText = styled.div`
+  margin: 20px auto 0 auto;
+  width: 868px;
+  font-size: 21px;
+  @media only screen and (max-width: 430px) {
+    width: 90%;
+  }
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background-color: rgb(0 0 0 / 20%);
+`;
+export const Text = styled.div`
+  font-size: 21px;
+  line-height: 142.5%;
+  & img {
+    margin: 0 auto;
+    width: 100%;
+    object-fit: cover;
+  }
+  & p {
+    margin: 1.5rem 0;
+  }
+  @media only screen and (max-width: 430px) {
+    font-size: 16px;
+  }
+`;
