b555bd45cd264b41dbeff1916284da92fbe83b70
jsango2 <jure.sango@gmail.com>
2022-08-23 11:01:53 +0200

    izmjena utrke pripremi se

--- pages/_document.js
+++ pages/_document.js
@@ -1,62 +1,35 @@
-// import Document, {
-//   Html,
-//   Head,
-//   Main,
-//   NextScript,
-//   DocumentContext,
-// } from "next/document";
-// import { ServerStyleSheet } from "styled-components";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-// export default class MyDocument extends Document {
-//   static async getInitialProps(ctx) {
-//     const sheet = new ServerStyleSheet();
-//     const originalRenderPage = ctx.renderPage;
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage;
 
-//     try {
-//       ctx.renderPage = () =>
-//         originalRenderPage({
-//           enhanceApp: (App) => (props) =>
-//             sheet.collectStyles(<App {...props} />),
-//         });
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      });
 
-//       const initialProps = await Document.getInitialProps(ctx);
-//       return {
-//         ...initialProps,
-//         styles: (
-//           <>
-//             {initialProps.styles}
-//             {sheet.getStyleElement()}
-//           </>
-//         ),
-//       };
-//     } finally {
-//       sheet.seal();
-//     }
-//   }
-//   render() {
-//     return (
-//       <Html lang="hr">
-//         <Head>
-//           <link
-//             rel="stylesheet"
-//             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
-//           ></link>
-//           <link
-//             rel="stylesheet"
-//             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
-//           ></link>
-//           <link
-//             href="https://fonts.googleapis.com/css2?family=Bangers&family=Montserrat:wght@400;500;600;700;900&display=swap"
-//             rel="stylesheet"
-//           ></link>
-//         </Head>
-//         {/* <title>Your site title</title> */}
-//         {this.props.styleTags}
-//         <body>
-//           <Main />
-//           <NextScript />
-//         </body>
-//       </Html>
-//     );
-//   }
-// }
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
